886dac9061723668aaa631827f376a87e4337f9d
moritatooru <tmtm1331@yahoo.ne.jp>
2023-10-10 19:41:09 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
